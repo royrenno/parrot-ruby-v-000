23d171066ce25cdc17c076b9bd88c4987d2966a9
roy renno <royrenno@yahoo.com>
2019-01-06 00:43:56 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts "Squawk!"
-  return "Squawk!"
-  puts "parrot"
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"  
 end
